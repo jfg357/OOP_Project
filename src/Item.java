4c495f38172377621669ef7c618cf06197cefc13
Juan Gaviria <jufgav@gmail.com>
2018-09-23 10:44:33 -0400

    Step 1 - fix format

--- src/Item.java
+++ src/Item.java
@@ -1,14 +1,26 @@
+/*
+ * Author: Juan Gaviria
+ * Item Java Interface to improve rapid development
+ * The Interface is a group of related methods with empty bodies.
+ * Interfaces form a contract between the class and the outside world, and this contract is enforced
+ * at build time by the compiler. All methods defined by that interface must appear in its source
+ * code before the class will successfully compile.
+*/
+
 import java.util.*;
 
 public interface Item {
+
   String manufacturer = "OracleProduction";
+
   String getName();
+
   Date getManufactureDate();
 
   void setProductionNumber(int pn);
-  void setName(String name);
-  int getSerialNumber();
 
-  public void pedalPress();
+  void setName(String name);
+
+  int getSerialNumber();
 
 }
--- src/ItemType.java
+++ src/ItemType.java
@@ -1,3 +1,12 @@
+/*
+ * Author: Juan Gaviria
+ * Java Enumerator to improve rapid development
+ * The Interface is a group of related methods with empty bodies.
+ * Interfaces form a contract between the class and the outside world, and this contract is enforced
+ * at build time by the compiler. All methods defined by that interface must appear in its source
+ * code before the class will successfully compile.
+ */
+
 public enum ItemType {
 
 }
