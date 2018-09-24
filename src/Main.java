c26822da72d27082cb0e12aa4d654382df4cb0a1
Juan Gaviria <jufgav@gmail.com>
2018-09-24 19:14:55 -0400

    Step 3a - Remove extra imports

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 import java.util.concurrent.*;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class Main {
 
   public static void main(String[] args) throws InterruptedException {
     Product prod1 = new Product("Product Name");
     System.out.println(prod1.toString());
-    TimeUnit.SECONDS.sleep(5);
+    TimeUnit.SECONDS.sleep(3);
     Product prod2 = new Product("DVD");
     System.out.println(prod1.toString());
     TimeUnit.SECONDS.sleep(2);
