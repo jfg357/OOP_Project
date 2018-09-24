2978ccad655b2029e91d5de4133d8eac97205740
Juan Gaviria <jufgav@gmail.com>
2018-09-24 19:10:31 -0400

    Step 3a - Add waiting time to test Date function

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -5,11 +6,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     Product prod1 = new Product("Product Name");
     System.out.println(prod1.toString());
+    TimeUnit.SECONDS.sleep(5);
     Product prod2 = new Product("DVD");
     System.out.println(prod1.toString());
+    TimeUnit.SECONDS.sleep(2);
     Product prod3 = new Product("CD");
     System.out.println(prod1.toString());
   }
