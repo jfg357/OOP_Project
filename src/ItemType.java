e5dd5775ac13cf2efe2afba8dee06e6bd6c17ae4
Juan Gaviria <jufgav@gmail.com>
2018-09-28 09:13:50 -0400

    Step 0 - Update Java Documentation

--- src/ItemType.java
+++ src/ItemType.java
@@ -1,10 +1,13 @@
-/*
- * Author: Juan Gaviria
- * Java Enumerator to improve rapid development
- * The Interface is a group of related methods with empty bodies.
- * Interfaces form a contract between the class and the outside world, and this contract is enforced
- * at build time by the compiler. All methods defined by that interface must appear in its source
- * code before the class will successfully compile.
+/**
+ * <ul>
+ *  <li>Author: Juan Gaviria</li>
+ *  <li>Java Enumerator to improve rapid development
+ *    The Interface is a group of related methods with empty bodies.
+ *    Interfaces form a contract between the class and the outside world, and this contract is enforced
+ *    at build time by the compiler. All methods defined by that interface must appear in its source
+ *    code before the class will successfully compile.
+ *  </li>
+ * </ul>
  */
 
 public enum ItemType {
