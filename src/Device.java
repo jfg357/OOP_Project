e5ee9c09c1094a7d1af4747578a5354f4fcf49f1
Juan Gaviria <jufgav@gmail.com>
2018-09-29 08:57:09 -0400

    Step 3 - Create Device Class that extends Abstract Class Product for testing purposes.

---
+++ src/Device.java
@@ -0,0 +1,15 @@
+public class Device extends Product{
+
+  /**
+   *
+   * @param name
+   */
+  Device(String name) {
+    super(name);
+  }
+
+  @Override
+  public void setProductionNumber(int pn) {
+
+  }
+}
