10478ba5909945259140c138ed793691b4048b24
Juan Gaviria <jfg357@gmail.com>
2018-10-27 19:09:36 -0400

    Add classes and interfaces

---
+++ src/AudioPlayer.java
@@ -0,0 +1,36 @@
+public class AudioPlayer extends Product implements MultimediaControl {
+
+  private String audioSpecification;
+  private ItemType mediaType;
+
+  AudioPlayer(){
+
+  }
+
+  AudioPlayer(String name, String audioSpecification){
+
+
+  }
+
+  @Override
+  public void play() {
+
+  }
+
+  @Override
+  public void stop() {
+
+  }
+
+  @Override
+  public void previous() {
+
+  }
+
+  @Override
+  public void next() {
+
+  }
+
+
+}
---
+++ src/MultimediaControl.java
@@ -0,0 +1,10 @@
+public interface MultimediaControl {
+
+  public void play();
+
+  public void stop();
+
+  public void previous();
+
+  public void next();
+}
