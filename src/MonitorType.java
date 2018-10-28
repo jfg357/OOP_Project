2a08b3b8ca696526589634e0a8f8d7de42bbf7af
Juan Gaviria <jfg357@gmail.com>
2018-10-27 20:49:07 -0400

    Step 7 & 8

--- src/MonitorType.java
+++ src/MonitorType.java
@@ -1,3 +1,22 @@
-public class MonitorType {
+/*
+ * Copyright (c) 2010, 2013, 2018 Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ */
 
+/**
+ * <h1>Java Enum Type</h1>
+ * The <b>Enum</b> type is a special data type that enables for a variable to be a set of predefined
+ * constants. The variable must be equal to one of the values that have been predefined for it.
+ * Because they are constants, the names of an enum type's fields are in uppercase letters.
+ *
+ * <p>This is a <a href="package-summary.html">enum class</a>
+ *
+ * @author Juan Gaviria
+ * @version 0.1
+ * @serial
+ * @since 0.2
+ */
+public enum MonitorType {
+  LCD, LED
 }
--- src/MultimediaControl.java
+++ src/MultimediaControl.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2010, 2013, 2018 Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ */
+
+/**
+ * <h1>MultimediaControl Interface</h1>
+ * Item Java <b>Interface</b> to improve rapid development The Interface is a group of related
+ * methods with empty bodies.
+ * <b>Interface</b> form a contract between the class and the outside world, and this
+ * contract is enforced at build time by the compiler.
+ * All methods defined by that interface must appear in its source code before the class will
+ * successfully compile.
+ *
+ * <p>This is a <a href="package-summary.html">interface class</a></p>
+ *
+ * @author Juan Gaviria
+ * @version 0.1
+ * @serial
+ * @since 0.2
+ */
+
+// Create the Item interface
 public interface MultimediaControl {
 
   public void play();
