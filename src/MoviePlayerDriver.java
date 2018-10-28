8414cea51f196dbb5ddb89162919aec5ee7aaa61
Juan Gaviria <jfg357@gmail.com>
2018-10-27 22:49:03 -0400

    Step 12 - Fix output

--- src/MoviePlayerDriver.java
+++ src/MoviePlayerDriver.java
@@ -23,11 +23,6 @@ public class MoviePlayerDriver {
 
   public static void testMoviePlayer() {
 
-    System.out.println("Playing");
-    System.out.println("Stopping");
-    System.out.println("Next");
-    System.out.println("Previous");
-
     System.out.println(new MoviePlayer("DBPOWER MK101",new Screen("720x480", 40,22), MonitorType.LCD));
     System.out.println(new MoviePlayer("Pyle PDV156BK",new Screen("1366x768", 40,22), MonitorType.LED));
 
