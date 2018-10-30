3d30cf1dec687b3690cefb11aa98c9f6eb1e04c3
Juan Gaviria <jfg357@gmail.com>
2018-10-30 08:03:57 -0400

    Add information to FX controller

--- src/fx/Controller.java
+++ src/fx/Controller.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2010, 2013, 2018 Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ */
+
+/**
+ * <h1>Java FX Controller</h1>
+ * The <b>Controller</b> is where the Java-FX magic happens.
+ *
+ * <p>This is a <a href="package-summary.html">fx class</a>
+ *
+ * @author Juan Gaviria
+ * @version 0.1
+ * @serial
+ * @since 0.2
+ */
 package fx;
 
 public class Controller {
